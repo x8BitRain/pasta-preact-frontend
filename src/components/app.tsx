28a1109c95aad225db9994502169d8b3803d909a
x8BitRain <dbell@btr.pm>
2020-05-10 00:46:23 +0200

    finally getting bidirectional websocket comms working

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,20 +1,20 @@
+/* eslint-disable */
 import { FunctionalComponent, h } from "preact";
+import Testing from "./testing";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
-    // tslint:disable-next-line:no-var-requires
-    require("preact/debug");
+  // tslint:disable-next-line:no-var-requires
+  require("preact/debug");
 }
 
 const App: FunctionalComponent = () => {
-    let currentUrl: string;
-    const handleRoute = (e: RouterOnChangeArgs) => {
-        currentUrl = e.url;
-    };
+	let currentUrl: string;
 
     return (
         <div id="app">
             <p>TEST</p>
+						<Testing />
         </div>
     );
 };
---
+++ src/components/testing.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable */
+import { Component } from "preact";
+// import { consumer, makePaste } from "../util/websocket";
+import cable from "actioncable";
+
+class Testing extends Component {
+  constructor() {
+    super();
+    this.state = {
+      uid: "cf76822f-b97f-4946-8bfe-84ced8717e2b",
+      token: "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiY2Y3NjgyMmYtYjk3Zi00OTQ2LThiZmUtODRjZWQ4NzE3ZTJiIiwiZXhwIjoxNTg5MTE2ODc1fQ.wL8HAQkHFX8Sg31Um3JXTKO4AmK3Fz8LZkTyghJG82w",
+      pasteParams: {
+        command: "message",
+        action: "speak",
+        data: {
+          content: "PASTA USER 1",
+          content_type: "text",
+          device: "pixel2xl"
+        }
+      }
+    };
+  }
+
+  openSocket = () => {
+    this.socket = cable.createConsumer(
+      `ws://localhost:3000/live/?uid=${this.state.uid}&token=${this.state.token}`
+    );
+    console.log("CREATED SOCKET")
+  }
+
+  subscribeConsumer = () => {
+    this.consumer = this.socket.subscriptions.create({
+      channel: "PasteChannel",
+      room_id: "9a446ad3-1c34-43f2-9f93-dcd74de5059c"
+    });
+  };
+
+
+  makePaste = () => {
+    this.consumer.send(this.state.pasteParams);
+  }
+
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    let time = new Date(this.state.time).toLocaleTimeString();
+    return (
+      <fragment>
+        <span>{time}</span>
+        <button onclick={this.openSocket}>OPENSOCKET</button>
+        <button onclick={this.subscribeConsumer}>SUBSCRIBE</button>
+        <button onclick={this.makePaste}>MAKE PASTE</button>
+      </fragment>
+    );
+  }
+}
+
+export default Testing;
