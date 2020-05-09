bfad252ec9497b77110278ed45b27c0827dbf94c
x8BitRain <dbell@btr.pm>
2020-05-09 21:12:10 +0200

    remove uneeded stuff and use SCSS

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,4 @@
 import { FunctionalComponent, h } from "preact";
-import { Route, Router, RouterOnChangeArgs } from "preact-router";
-
-import Home from "../routes/home";
-import Profile from "../routes/profile";
-import NotFoundPage from '../routes/notfound';
-import Header from "./header";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 if ((module as any).hot) {
@@ -20,13 +14,7 @@ const App: FunctionalComponent = () => {
 
     return (
         <div id="app">
-            <Header />
-            <Router onChange={handleRoute}>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
+            <p>TEST</p>
         </div>
     );
 };
