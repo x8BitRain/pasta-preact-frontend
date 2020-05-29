cee727272f187ff055f6c8ae64f5a6bfc8df7e98
x8BitRain <dbell@btr.pm>
2020-05-29 18:18:07 +0200

    add scroll to bottom function but need to wait for proper scroll height thingo to be set

---
+++ src/util/scrollToBottom.tsx
@@ -0,0 +1,6 @@
+const scrollToBottom = element => {
+  const pasteList = document.querySelector("#paste-list")
+  pasteList.scrollTo({ top: pasteList.scrollHeight, behavior: "smooth" });
+};
+
+export default scrollToBottom;
